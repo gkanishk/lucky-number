4497746f41c6cd79244f9d344f5977cfb61ce278
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-08-01 12:10:50 +0530

    feat: Completed mark 11

---
+++ dist/hq-sw.js
@@ -0,0 +1,16 @@
+
+const cacheName = 'hq-1627748877087';
+const appShellFiles = ['/index.html','/style.css','/./index.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(appShellFiles)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keyList => Promise.all(keyList.map(key => caches.delete(key)))));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request)
+    .then(r => r || fetch(e.request)));
+});
